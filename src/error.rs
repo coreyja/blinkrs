51ae7af4ee81ff58963bc419a9e207323bcb61cd
Corey Alexander <coreyja@gmail.com>
2021-11-06 00:24:43 -0400

    Use thiserror for its Error derive

--- src/error.rs
+++ src/error.rs
@@ -1,33 +1,14 @@
-use std::error::Error;
 use std::fmt;
+use thiserror::Error;
 
 /// An Error from the USB Protocol
-#[derive(Debug)]
+#[derive(Debug, Error)]
 pub enum BlinkError {
   /// Could not find something.... Maybe a USB interface?
   /// TODO: Make this doc better
+  #[error("not found")]
   NotFound,
   /// Could not list USB devices, wrapbs an rusb::Error
-  DeviceListError(rusb::Error),
-}
-
-impl From<rusb::Error> for BlinkError {
-  fn from(error: rusb::Error) -> Self {
-    BlinkError::DeviceListError(error)
-  }
-}
-
-impl fmt::Display for BlinkError {
-  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-    write!(f, "{}", self.description())
-  }
-}
-
-impl Error for BlinkError {
-  fn description(&self) -> &str {
-    match self {
-      BlinkError::NotFound => "not found",
-      BlinkError::DeviceListError(_e) => "unable to find usb device",
-    }
-  }
+  #[error("device list error")]
+  DeviceListError(#[from] rusb::Error),
 }
